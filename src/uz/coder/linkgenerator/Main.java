4e079e414b42462544e2fbbf5a32ee53bf2a2449
mrazamat <azamat00189@mail.ru>
2018-09-14 18:31:25 +0500

    changed any funcsional

--- src/uz/coder/linkgenerator/Main.java
+++ src/uz/coder/linkgenerator/Main.java
@@ -5,6 +5,19 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Config cfg = new Config();
-        System.out.println(cfg.getPropValues());
+        String str = cfg.getPropValues();
+        int count = Integer.parseInt(str.substring(0, 2));
+        String url = str.substring(2);
+        url=url.trim();
+
+
+
+        for (int i = 1; i < count; i++) {
+            System.out.println("<p>" +
+                    "<a href=\""  + url + i +".jpg\" data-fancybox=\"gallery\">" +
+                    "<img class=\"img-fluid\" src=\"" + url + i +".jpg\"/>" +
+                    "</a>" +
+                    "</p>\n");
+        }
     }
 }
